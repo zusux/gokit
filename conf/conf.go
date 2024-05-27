05671419a8d4cbad92b704cf6bf96026cac0eae7
DESKTOP-4AEUJ6R\xu <zhxx136@live.com>
2024-05-27 16:08:18 +0800

    -

--- conf/conf.go
+++ conf/conf.go
@@ -1,17 +1,18 @@
 package conf
 
 import (
-	"gopkg.in/yaml.v3"
+	"github.com/goccy/go-yaml"
 	"log"
 	"os"
 )
 
-func MustLoad(path string, v *any) {
+func MustLoad(path string, v any) {
 	file, err := os.ReadFile(path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = yaml.Unmarshal(file, &v)
+
+	err = yaml.Unmarshal(file, v)
 	if err != nil {
 		log.Fatal(err)
 	}
