e16b63f08dc0ef527a561304afa66a4733ab6799
DESKTOP-4AEUJ6R\xu <zhxx136@live.com>
2024-05-27 15:52:02 +0800

    -

--- conf/conf.go
+++ conf/conf.go
@@ -6,12 +6,12 @@ import (
 	"os"
 )
 
-func MustLoad(path string, v any) {
+func MustLoad(path string, v *any) {
 	file, err := os.ReadFile(path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = yaml.Unmarshal(file, v)
+	err = yaml.Unmarshal(file, &v)
 	if err != nil {
 		log.Fatal(err)
 	}
