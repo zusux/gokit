56cd188ef260ac9675cb1bbe93ac7ef86b20ccba
DESKTOP-4AEUJ6R\xu <zhxx136@live.com>
2025-09-12 01:28:18 +0800

    mongo

--- mon/mongo.go
+++ mon/mongo.go
@@ -2,6 +2,9 @@ package mon
 
 import (
 	"context"
+	"net/url"
+	"time"
+
 	"github.com/zusux/gokit/zlog"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -10,17 +13,21 @@ import (
 func (m *ConfigMongo) Init(ctx context.Context) *mongo.Client {
 	// 替换成你的 MongoDB 地址
 	if m.Enable {
-		clientOptions := options.Client().ApplyURI(m.Uri)
+		clientOptions := options.Client().ApplyURI(url.QueryEscape(m.Uri)).
+			SetConnectTimeout(5 * time.Second).
+			SetServerSelectionTimeout(5 * time.Second)
 		// 连接
 		client, err := mongo.Connect(ctx, clientOptions)
 		if err != nil {
-			zlog.Fatalf("连接失败: err:%v", err)
+			zlog.Errorf("MongoDB连接失败 err:%v", err.Error())
+			return nil
 		}
 
 		// Ping 测试
 		err = client.Ping(ctx, nil)
 		if err != nil {
-			zlog.Fatalf("Ping失败: err:%v", err)
+			zlog.Errorf("MongoDB Ping失败 err:%v", err.Error())
+			return nil
 		}
 		zlog.Info("MongoDB连接成功！")
 		return client
